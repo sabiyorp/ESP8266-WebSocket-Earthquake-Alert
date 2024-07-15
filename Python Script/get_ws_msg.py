4f19532764ce514bd86ccbaed714f7284c25ed28
sabiyorp <sabiyorp@outlook.com>
2024-07-15 19:00:33 +0700

    Add files via upload

---
+++ Python Script/get_ws_msg.py
@@ -0,0 +1,15 @@
+# Created by: Sabiyo Riezky Pratama.
+# License: Public Domain (Creative Commons Zero v1.0 Universal)
+
+import asyncio
+import websockets
+
+async def receive_message():
+    uri = "ws://<ESP8266_IP_ADDRESS>:80"  # Replace <ESP8266_IP_ADDRESS> with the actual IP address of your ESP8266
+    async with websockets.connect(uri) as websocket:
+        while True:
+            message = await websocket.recv()
+            print(f"Received message: {message}")
+
+if __name__ == "__main__":
+    asyncio.run(receive_message())
