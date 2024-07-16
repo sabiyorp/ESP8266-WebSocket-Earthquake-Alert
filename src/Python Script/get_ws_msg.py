4905092d45bd7fca1e3201af91e1348c0520f218
sabiyorp <sabiyorp@outlook.com>
2024-07-16 17:45:42 +0700

    Update get_ws_msg.py

--- src/Python Script/get_ws_msg.py
+++ src/Python Script/get_ws_msg.py
@@ -1,15 +1,32 @@
-# Created by: Sabiyo Riezky Pratama
-# License: Public Domain (Creative Commons Zero v1.0 Universal)
+'''
+Created by: Sabiyo Riezky Pratama
+License: Public Domain (Creative Commons Zero v1.0 Universal)
+
+Based on the WebSocket server example by Shawn Hymel
+Original code: https://shawnhymel.com/1675/arduino-websocket-server-using-an-esp32/
+'''
 
 import asyncio
 import websockets
 
 async def receive_message():
-    uri = "ws://<ESP8266_IP_ADDRESS>:80"  # Replace <ESP8266_IP_ADDRESS> with the actual IP address of your ESP8266
-    async with websockets.connect(uri) as websocket:
-        while True:
-            message = await websocket.recv()
-            print(f"Received message: {message}")
+    uri = "ws://192.168.XXX.XXX"  # Replace with the actual IP address of your ESP8266
+    while True:
+        try:
+            async with websockets.connect(uri) as websocket:
+                while True:
+                    try:
+                        message = await asyncio.wait_for(websocket.recv(), timeout=2.0)
+                        print(f"Received message: {message}")
+                    except asyncio.TimeoutError:
+                        print("Reconnecting...")
+                        break
+        except (websockets.exceptions.ConnectionClosedError, websockets.exceptions.InvalidURI) as e:
+            print(f"Connection error: {e}. Reconnecting...")
+            await asyncio.sleep(2)  # Wait before retrying the connection
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}. Reconnecting...")
+            await asyncio.sleep(2)  # Wait before retrying the connection
 
 if __name__ == "__main__":
     asyncio.run(receive_message())
